9a8bf972f581eb90ba438f24647962548c228f11
Lê Bảo Long <longle12042006a@gmail.com>
2024-07-12 07:38:09 +0700

    upload

--- CodeBotB.ino
+++ CodeBotB.ino
@@ -2,33 +2,22 @@
 #include <Arduino.h>
 
 // Define pin numbers
-const int SERVO_PIN = 9;
-const int TRIGGER_PIN = 7;
-const int ECHO_PIN = 6;
-// const int openPosition;
-// const int closePosition;
-bool flash = true;  //MoThungRac();
+const int SERVO_PIN       =;
+const int TRIGGER_PIN     =;
+const int ECHO_PIN        =;
+const int openPosition    =;
+const int closePosition   =;
+const int interval        =;
+bool flash = true; // MoThungRac();
 
 // Create an instance of the BotBrainSmartBin class
-BotBrainSmartBin smartBin(SERVO_PIN, TRIGGER_PIN, ECHO_PIN);
+BotBrainSmartBin smartBin(SERVO_PIN, TRIGGER_PIN, ECHO_PIN, openPosition, closePosition, interval);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
-  smartBin.setupAllSmartBin(SERVO_PIN, TRIGGER_PIN, ECHO_PIN);
-  smartBin.DongThungRac();
 }
 
-void loop() {
-  // Call getDistance to get the distance from the ultrasonic sensor
-  float distance = smartBin.getDistance();
-  if (smartBin.timeToUpdate()) {
-    // Serial.println(distance);
-    if (distance < 15 && flash) {
-      smartBin.MoThungRac();  // Open the bin lid
-      flash = false;          // Update the check flag
-    } else if (distance > 90) {
-      smartBin.DongThungRac();  // Close the bin lid
-      flash = true;            // Update the check flag
-    }
-  }
+void loop()
+{
 }
